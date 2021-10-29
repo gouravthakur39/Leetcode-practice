f0f654ceb2b8b9b11ca306e5a716ffcc7eca634f
gouravthakur39 <gourav.thakur39@gmail.com>
2021-10-29 16:09:48 +0530

    practice

---
+++ longestPalindromicSubstring.js
@@ -0,0 +1,20 @@
+var longestPalindrome = function(s) {
+    let longest = '';
+    
+    for (let i=0; i<s.length; i++){
+        expandCheck(i, i);
+        expandCheck(i, i+1);
+    }
+    
+    function expandCheck(l, r){
+        while (l>=0 && r<s.length && s[l]===s[r]){
+            if (r-l+1 > longest.length){
+                longest = s.slice(l, r+1);
+            }
+            l--;
+            r++;
+        }
+    }
+    
+    return longest;
+};
